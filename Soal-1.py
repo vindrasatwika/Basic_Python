60b5efe7f1bba241a8f00adca48f7a14257337d3
vindrasatwika <80442282+vindrasatwika@users.noreply.github.com>
2021-03-17 22:15:34 +0700

    Delete Soal-1.py

--- Soal-1.py
+++
@@ -1,4 +0,0 @@
-nama = str(input("Masukkan nama anda: "))
-umur = int(input("Masukkan umur anda: "))
-tinggi = float(input("Masukkan tinggi badan anda: "))
-print("Nama saya ",nama,", umur saya",umur,"tahun dan tinggi saya",tinggi,"cm.")
